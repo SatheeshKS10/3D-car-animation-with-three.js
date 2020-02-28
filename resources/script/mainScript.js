5b58967acdc1c0d6b281d4a9a24e1049eb9f2b64
Satheesh K <satheeshvsbk@gmail.com>
2020-02-28 21:40:03 +0530

    updated correct path

--- resources/script/mainScript.js
+++ resources/script/mainScript.js
@@ -67,8 +67,8 @@ function init(){
 	// https://satheeshks10.github.io/Three.js-basic3DObjCarModel/resources/script/mainScript.js
 	function loadSedan(){
 	var mtlLoader = new THREE.MTLLoader();
-	mtlLoader.setTexturePath('Three.js-basic3DObjCarModel/resources/carModel/sedan/');
-	mtlLoader.setPath('/resources/carModel/sedan/');
+	//mtlLoader.setTexturePath('Three.js-basic3DObjCarModel/resources/carModel/sedan/');
+	mtlLoader.setPath('Three.js-basic3DObjCarModel/resources/carModel/sedan/');
 	mtlLoader.load('sedan.mtl', function(materials) {
 
 		var objLoader = new THREE.OBJLoader();
